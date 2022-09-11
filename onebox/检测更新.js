383c142f73e79966e860c52d943ae43abc359e76
1260957445@qq.com <1260957445@qq.com>
2022-09-11 16:45:48 +0800

    g

---
+++ onebox/检测更新.js
@@ -0,0 +1,11 @@
+<a class="版本">220911</a>
+<ul class="默认提示">
+<li><font color="blue"><b>已是最新版，本轻站适用最新版土狗浏览器</b></font>
+<p>mqqwpa://im/chat?chat_type=wpa&uin=1260957445</p>
+</li>
+</ul>
+<ul class="更新提示">
+<li><font color="red"><b>有新版本!!![]点击安装</b></font>
+<p>mapi:qm/https://gitcode.net/egwang186/iptv/-/raw/master/onebox/一个盒子.mkz</p>
+</li>
+</ul>
