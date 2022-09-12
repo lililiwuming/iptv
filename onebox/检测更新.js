477f12b4726e049677d9294861bac36a15129755
1260957445@qq.com <1260957445@qq.com>
2022-09-12 19:24:08 +0800

    g

--- onebox/检测更新.js
+++ onebox/检测更新.js
@@ -1,3 +1,4 @@
+<html>
 <a class="版本">2209123</a>
 <ul class="默认提示">
 <li><font color="blue"><b>已是最新版，本轻站适用最新版土狗浏览器</b></font>
@@ -9,3 +10,4 @@
 <p>mapi:qm/https://gitcode.net/egwang186/iptv/-/raw/master/onebox/一个盒子.mkz</p>
 </li>
 </ul>
+</html>
