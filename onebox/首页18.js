2c3bdc50089d77d25daa8aa32bee7350bdaf564b
1260957445@qq.com <1260957445@qq.com>
2022-09-12 23:16:02 +0800

    g

---
+++ onebox/首页18.js
@@ -0,0 +1,282 @@
+#####18
+
+####
+###type
+web
+###分类
+普通站源
+###标题
+XChina
+###图片
+https://gitcode.net/egwang186/iptv/-/raw/master/onebox/xchina.png
+###BaseURL
+"https://xchina.xyz";
+###首页地址
+getVar("baseURL")+"/videos.html";
+###分类地址
+getVar("baseURL")+"/分类/翻页.html";
+###搜索地址
+getVar("baseURL")+"/videos/keyword-关键字/翻页.html";
+###rule
+##首页规则
+var 列表=e2Arr(getVar("源码"),".get(div.videos div.item)");var 标题规则=".get(div.text a).t()";var 地址规则=".get(div.text a).a(href)";var 图片规则=".get(video).a(poster)";var 简介规则=".get(div.tag).t()";var 图片底部规则=".get(div.duration)";var 左上规则=".tx(<p style='background-color:#7091fc'><font color='#FFFFFF' size='40px'>).get(div.series).t().ct(</font></p>)";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##筛选数据
+var 源码=getHttp(JSON.stringify({url:"https://xchina.xyz/videos.html",head:{"User-Agent":"Windows 10"}}));var Series=源码.match(/<a href="\/videos\/series-.+?<\/a>/g);var Model=源码.match(/<a href="\/videos\/model-.+?<\/a>/g);var a="";for(var i in Series){var title=e2Rex(Series[i],".t()");var id=e2Rex(Series[i],".ty(/).tz(.html)");a=a+"+"+title+"="+id;}for(var i in Model){var title=e2Rex(Model[i],".t()");var id=e2Rex(Model[i],".ty(/).tz(.html)");a=a+"+"+title+"="+id;}var a="分类"+a;var b="";for(var i=1;i<50;i++){b=b+"+第"+i+"页="+i;}var b="翻页+"+b;a+"\n"+b;
+##分类规则
+var 列表=e2Arr(getVar("源码"),".get(div.videos div.item)");var 标题规则=".get(div.text a).t()";var 地址规则=".get(div.text a).a(href)";var 图片规则=".get(video).a(poster)";var 简介规则=".get(div.tag).t()";var 图片底部规则=".get(div.duration)";var 左上规则=".tx(<p style='background-color:#7091fc'><font color='#FFFFFF' size='40px'>).get(div.series).t().ct(</font></p>)";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##选集规则
+var 分类=e2Arr(getVar("源码"),".ty(var hlsUrl).ty(\").tz(\")");var 简介=getVar("msg");var 线路="";var 列表规则=".t()";var 标题规则=getVar("name");var 选集规则=".t()";var 选集地址规则=".t()";
+##搜索规则
+var 源码=getVar("源码");var 列表=e2Arr(源码,".get(div.videos div.item)");if(!列表[0]){var 列表=["网页可能有搜索验证，去网页看看吧"];var 标题规则=".t()";var 地址规则=getVar("baseURL");var 图片规则=".get(img).a(data-original)";var 简介规则="长按网页打开，验证后返回重新搜索即可";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}else{var 标题规则=".get(div.text a).t()";var 地址规则=".get(div.text a).a(href)";var 图片规则=".get(video).a(poster)";var 简介规则=".get(div.tag).t()";var 图片底部规则=".get(div.duration)";var 左上规则=".get(div.series)";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}
+##搜索翻页
+var b="";for(var i=1;i<50;i=i+1){b=b+"+第"+i+"页="+i;}
+"翻页"+b;
+##免嗅探规则
+JSON.stringify({url:getVar("地址"),head:{"Referer":"https://xchina.xyz"}});
+####
+
+####
+###type
+web
+###分类
+普通站源
+###标题
+搬淫家
+###图片
+https://gitcode.net/egwang186/iptv/-/raw/master/onebox/byj.png
+###BaseURL
+var code=getHttp("https://byjav.me/js/home.js");e2Rex(code,".ty(byjios.).th(http://##https://).z(http.*me).or().z(http.*com).or().z(http.*tv)");
+###首页地址
+getVar("baseURL")+"/index/home.html";
+###分类地址
+getVar("baseURL")+"分类翻页";
+###搜索地址
+getVar("baseURL")+"/search/video/?s=关键字&page=翻页";
+###rule
+##首页规则
+var 列表=e2Arr(getVar("源码"),".get(div.cell.video)");var 标题规则=".get(h5).t()";var 地址规则=".get(a).a(href)";var 图片规则=".get(img).a(data-src)";var 简介规则=".get(span.badge).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##筛选数据
+var code=getHttp(getVar("baseURL")+"/community/");var arr=e2Arr(code,".get(div.cell.category)");var sort=["分类"];for(let i=0;i<arr.length;i++){let a=e2Rex(arr[i],".get(h5).t().ct(=)");let b=e2Rex(arr[i],".get(a).href()");sort.push(a+b);};var pn=["翻页+第1页="];for(let i=2;i<=999;i++){let a="第"+i+"页="let b=i+"/";pn.push(a+b);};sort.join("+")+"\n"+pn.join("+");
+##分类规则
+var 列表=e2Arr(getVar("源码"),".get(div.cell.video)");var 标题规则=".get(h5).t()";var 地址规则=".get(a).a(href)";var 图片规则=".get(img).a(data-src)";var 简介规则=".get(span.badge).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##选集规则
+var uri=e2Rex(getVar("源码"),".get(div#player-fluid).a(style).z(/.+/)");var uri="https://storage.banyinjia8.com"+uri.replace("tmb","hls");var hd=getVar("msg");var list=["1080p","720p","480p","playlist"];var title=["1080p","720p","480p","默认"];var j=list.indexOf(hd)>-1?list.indexOf(hd):0;var res=[];for(var i=j;i<list.length;i++){var 标题=title[i];var 地址=uri+list[i]+".m3u8";res.push({name:标题,url:地址});};var 分类=e2Arr(JSON.stringify(res),".c()");;var 简介="";var 线路="";var 列表规则=".json()";var 标题规则=".var(name)";var 选集规则=".json(name)";var 选集地址规则=".json(url)";
+##搜索规则
+var 源码=getVar("源码");var 列表=e2Arr(源码,".get(div.cell.video)");if(!列表[0]){var 列表=["网页可能有搜索验证，去网页看看吧"];var 标题规则=".t()";var 地址规则=getVar("baseURL");var 图片规则=".get(img).a(data-original)";var 简介规则="长按网页打开，验证后返回重新搜索即可";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}else{var 标题规则=".get(h5).t()";var 地址规则=".get(a).a(href)";var 图片规则=".get(img).a(data-src)";var 简介规则=".get(span.badge).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}
+##搜索翻页
+var b="";for(var i=1;i<50;i=i+1){b=b+"+第"+i+"页="+i;}
+"翻页"+b;
+##免嗅探规则
+JSON.stringify({url:getVar("地址")});
+####
+
+####
+###type
+web
+###分类
+普通站源
+###标题
+直播盒子
+###图片
+https://gitcode.net/egwang186/iptv/-/raw/master/onebox/zhibohezi.png
+###BaseURL
+"http://api.vipmisss.com:81";
+###首页地址
+"http://api.hclyz.com:81/mf/json.txt";
+###分类地址
+getVar("baseURL")+"/分类/json.txt翻页";
+###搜索地址
+"http://ip111.cn/?wd=关键字翻页";
+###rule
+##首页规则
+var 列表=e2Arr(getVar("源码"),".json(pingtai)");var 标题规则=".json(title)";var 地址规则=".c(http://api.hclyz.com:81/mf/).json(address)";var 图片规则=".json(xinimg)";var 简介规则=".json(Number).ct(位主播)";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##筛选数据
+var a="分类+xcdsw";var b="翻页+全部=";a+"\n"+b;
+##分类规则
+var 列表=e2Arr(getVar("源码"),".json(pingtai)");var 标题规则=".json(title)";var 地址规则=".c(/xcdsw/).json(address)";var 图片规则=".json(xinimg)";var 简介规则=".json(Number).ct(位主播)";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##选集规则
+var 分类=e2Arr(getVar("源码"),".all()");var 简介=getVar("name");var 线路="";var 列表规则=".json(zhubo)";var 标题规则=getVar("name");var 选集规则=".json(title)";var 选集地址规则=".c(http://ip111.cn/?wd=).json(address)";
+##搜索规则
+var 源码=getHttp(JSON.stringify({url:"http://api.vipmisss.com:81/xcdsw/json.txt"}));var 列表=e2Arr(源码,".json(pingtai)").filter(item=>item.indexOf(getVar("关键字"))!=-1);if(!列表[0]){var 列表=["未匹配到任何平台"];var 标题规则=".t()";var 地址规则=getVar("baseURL");var 图片规则=".get(img).a(data-original)";var 简介规则="未匹配到任何平台";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}else{var 标题规则=".json(title)";var 地址规则=".c(/xcdsw/).json(address)";var 图片规则=".json(xinimg)";var 简介规则=".json(Number).ct(位主播)";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}
+##搜索翻页
+var b="翻页+全部=";b;
+##免嗅探规则
+JSON.stringify({url:getVar("地址").split("http://ip111.cn/?wd=")[1]});
+####
+
+####
+###type
+web
+###分类
+普通站源
+###标题
+肉视频
+###图片
+https://gitcode.net/egwang186/iptv/-/raw/master/onebox/rouvideo.png
+###BaseURL
+"https://rou.video";
+###首页地址
+getVar("baseURL")+"/home";
+###分类地址
+getVar("baseURL")+"分类?page=翻页";
+###搜索地址
+getVar("baseURL")+"/search?q=关键字&page=翻页";
+###rule
+##首页规则
+var 列表=e2Arr(getVar("源码"),".get(div.shadow.p-1)");var 标题规则=".get(img).a(alt)";var 地址规则=".get(a).a(href)";var 图片规则=".get(img).a(src)";var 简介规则=".get(div.absolute).st().t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##筛选数据
+var code=getHttp(getVar("baseURL")+"/cat");var arr=e2Arr(code,".get(div.grid).i(0).get(a)");var sort=["分类+國產AV=/t/國產AV+自拍流出=/t/自拍流出+探花=/t/探花"];for(let i=0;i<arr.length;i++){let a=e2Rex(arr[i],".t().ct(=)");let b=e2Rex(arr[i],".href()");sort.push(a+b);};var pn=["翻页"];for(let i=1;i<=99;i++){let a="第"+i+"页="let b=i;pn.push(a+b);};sort.join("+")+"\n"+pn.join("+");
+##分类规则
+var 列表=e2Arr(getVar("源码"),".get(div.shadow.p-1)");var 标题规则=".get(img).a(alt)";var 地址规则=".get(a).a(href)";var 图片规则=".get(img).a(src)";var 简介规则=".get(div.absolute).st().t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##选集规则
+var 分类=e2Arr(getVar("源码"),".get(script).i(-1).t()");;var 简介="";var 线路="";var 列表规则=".json(props).json(pageProps).json(video)";var 标题规则=".var(name)";var 选集规则=".json(name)";var 选集地址规则=".json(videoUrl)";
+##搜索规则
+var 源码=getVar("源码");var 列表=e2Arr(源码,".get(div.shadow.p-1)");if(!列表[0]){var 列表=["网页可能有搜索验证，去网页看看吧"];var 标题规则=".t()";var 地址规则=getVar("baseURL");var 图片规则=".get(img).a(data-original)";var 简介规则="长按网页打开，验证后返回重新搜索即可";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}else{var 标题规则=".get(img).a(alt)";var 地址规则=".get(a).a(href)";var 图片规则=".get(img).a(src)";var 简介规则=".get(div.absolute).st().t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}
+##搜索翻页
+var b="";for(var i=1;i<50;i=i+1){b=b+"+第"+i+"页="+i;}
+"翻页"+b;
+##免嗅探规则
+var 头=getVar("地址").split("/hls")[0];var code=getHttp(getVar("地址")).match(/#EXT-X-STREAM-INF[^#]+/g);var playlist=[];for(var i in code){var name=code[i].match(/RESOLUTION=(.+)/)[1];var url=头+"/"+code[i].match(/hls.+/)[0];playlist.push({name:name,url:url});}JSON.stringify(playlist);
+####
+
+####
+###type
+web
+###分类
+普通站源
+###标题
+黄色仓库
+###图片
+https://gitcode.net/egwang186/iptv/-/raw/master/onebox/hsck.png
+###BaseURL
+var url="http://hsck.net/";var jx=e2Rex(getHttp(url),".z(http[^\"]+)");var code=JZ(JSON.stringify({url:jx+url,redirect:false}));code.head.Location;
+###首页地址
+getVar("baseURL")+"/";
+###分类地址
+getVar("baseURL")+"/vodtype/分类-翻页.html";
+###搜索地址
+getVar("baseURL")+"/vodsearch/关键字----------翻页---.html";
+###rule
+##首页规则
+var 列表=e2Arr(getVar("源码"),".get(ul.stui-vodlist li)");var 标题规则=".get(.title a).t()";var 地址规则=".get(.title a).a(href)";var 图片规则=".get(a).a(data-original)";var 简介规则=".get(span).st().t().c().get(p).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##筛选数据
+var a="分类+日韩AV=1+国产系列=2+欧美=3+成人动漫=4+中字无码=8+中字有码=9+日本无码=10+日本有码=7+国产视频=15+欧美高清=21+动漫剧情=22";var b="";for(var i=1;i<50;i++){b=b+"+第"+i+"页="+i;}var b="翻页+"+b;a+"\n"+b;
+##分类规则
+var 列表=e2Arr(getVar("源码"),".get(ul.stui-vodlist li)");var 标题规则=".get(.title a).t()";var 地址规则=".get(.title a).a(href)";var 图片规则=".get(a).a(data-original)";var 简介规则=".get(span).st().t().c().get(p).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##选集规则
+var 分类=e2Arr(getVar("源码"),".get(div.stui-warp-content)");var 线路="";var 简介=e2Rex(getVar("源码"),".get(div.stui-warp-content).t()");var 列表规则=".ty(player_aaaa=).json(url)";var 标题规则=".get(h3).t()";var 选集规则="播放";var 选集地址规则=".t()";
+##搜索规则
+var 源码=getVar("源码");var 列表=e2Arr(源码,".get(ul.stui-vodlist li)");if(!列表[0]){var 列表=["网页可能有搜索验证，去网页看看吧"];var 标题规则=".t()";var 地址规则=getVar("baseURL");var 图片规则=".get(img).a(data-original)";var 简介规则="长按网页打开，验证后返回重新搜索即可";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}else{var 标题规则=".get(.title a).t()";var 地址规则=".get(.title a).a(href)";var 图片规则=".get(a).a(data-original)";var 简介规则=".get(span).st().t().c().get(p).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}
+##搜索翻页
+var b="";for(var i=1;i<50;i=i+1){b=b+"+第"+i+"页="+i;}
+"翻页"+b;
+##免嗅探规则
+JSON.stringify({url:getVar("地址")});
+####
+
+####
+###type
+web
+###分类
+普通站源
+###标题
+JableTV
+###图片
+https://gitcode.net/egwang186/iptv/-/raw/master/onebox/jable.png
+###BaseURL
+"https://jable.tv";
+###首页地址
+getVar("baseURL")+"/";
+###分类地址
+getVar("baseURL")+"分类?mode=async&function=get_block&block_id=list_videos_common_videos_list&sort_by=post_date&from=翻页";
+###搜索地址
+getVar("baseURL")+"/search/关键字/?mode=async&function=get_block&block_id=list_videos_videos_list_search_result&q=关键字&sort_by=&from_videos=翻页";
+###rule
+##首页规则
+var 列表=e2Arr(getVar("源码"),".get(div.video-img-box.mb-e-20)");var 标题规则=".get(.title a).t()";var 地址规则=".get(.title a).a(href)";var 图片规则=".get(img).a(data-src)";var 简介规则=".t()";var 图片底部规则=".get(span.label).t()";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##筛选数据
+try{var 源码=getHttp("https://jable.tv/categories/");}catch(err){alert("哦，报错了，错误描述："+err.message);}var 标签列表=e2Arr(源码,".get(a.tag)");var 主题列表=e2Arr(源码,".get(div.video-img-box.mb-e-20)");var a="";for(var i in 主题列表){var title=e2Rex(主题列表[i],".get(h4).t()");var id=e2Rex(主题列表[i],".get(a).a(href).ty(jable.tv)");a=a+"+"+title+"="+id;}for(var i in 标签列表){var title=e2Rex(标签列表[i],".get(a).t()");var id=e2Rex(标签列表[i],".get(a).a(href).ty(jable.tv)");a=a+"+"+title+"="+id;}var a="分类+"+a;var b="";for(var i=1;i<50;i++){b=b+"+第"+i+"页="+i;}var b="翻页+"+b;a+"\n"+b;
+##分类规则
+var 列表=e2Arr(getVar("源码"),".get(div.video-img-box.mb-e-20)");var 标题规则=".get(.title a).t()";var 地址规则=".get(.title a).a(href)";var 图片规则=".get(img).a(data-src)";var 简介规则=".t()";var 图片底部规则=".get(span.label).t()";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##选集规则
+var 分类=getVar("源码").match(/<video[\s\S]+?</h4>/g);var 简介=e2Rex(getVar("源码"),".get(div.info-header)");var 线路="";var 列表规则=".z(var hlsUrl.+)";var 标题规则=".get(h4).t()";var 选集规则="播放";var 选集地址规则=".ty(').tz(')";
+##搜索规则
+var 源码=getVar("源码");var 列表=e2Arr(源码,".get(div.video-img-box.mb-e-20)");if(!列表[0]){var 列表=["网页可能有搜索验证，去网页看看吧"];var 标题规则=".t()";var 地址规则=URL;var 图片规则=".get(img).a(data-original)";var 简介规则="长按网页打开，验证后返回刷新即可";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}else{var 标题规则=".get(.title a).t()";var 地址规则=".get(.title a).a(href)";var 图片规则=".get(img).a(data-src)";var 简介规则=".t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}
+##搜索翻页
+var b="";for(var i=1;i<50;i=i+1){b=b+"+第"+i+"页="+i;}
+"翻页"+b;
+##免嗅探规则
+JSON.stringify({url:getVar("地址")});
+####
+
+####
+###type
+web
+###分类
+普通站源
+###标题
+CableAV
+###图片
+https://gitcode.net/egwang186/iptv/-/raw/master/onebox/cable.png
+###BaseURL
+"https://cableav.tv";
+###首页地址
+getVar("baseURL");
+###分类地址
+"分类翻页";
+###搜索地址
+getVar("baseURL")+"/page/翻页/?s=关键字";
+###rule
+##首页规则
+var 列表=e2Arr(getVar("源码"),".get(div.post-item-wrap)");var 标题规则=".get(h3).t()";var 地址规则=".get(a).a(href)";var 图片规则=".get(img).a(data-src)";var 简介规则=".get(div.categories-wrap).t().tx(&nbsp;).get(span.duration-text).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##筛选数据
+var code=getHttp(getVar("baseURL")+"/playlist/");var arr=e2Arr(code,".get(li.cat-item a)");var sort=["分类"];for(let i=0;i<arr.length;i++){let a=e2Rex(arr[i],".t().ct(=)");let b=e2Rex(arr[i],".href()");sort.push(a+b);};var pn=["翻页+第1页="];for(let i=2;i<=99;i++){let a="第"+i+"页="let b="page/"+i+"\\/";pn.push(a+b);};sort.join("+")+"\n"+pn.join("+");
+##分类规则
+var 列表=e2Arr(getVar("源码"),".get(div.post-item-wrap)");var 标题规则=".get(h3).t()";var 地址规则=".get(a).a(href)";var 图片规则=".get(img).a(data-src)";var 简介规则=".get(div.categories-wrap).t().tx(&nbsp;).get(span.duration-text).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##选集规则
+var 分类=e2Arr(getVar("源码"),".z(\[\{.*?source.*?\}\])");;var 简介="";var 线路="";var 列表规则=".json()";var 标题规则=".var(name)";var 选集规则=".json(source_label).th(Login##)";var 选集地址规则=".json(source_file)";
+##搜索规则
+var 源码=getVar("源码");var 列表=e2Arr(源码,".get(div.post-item-wrap)");if(!列表[0]){var 列表=["网页可能有搜索验证，去网页看看吧"];var 标题规则=".t()";var 地址规则=URL;var 图片规则=".get(img).a(data-original)";var 简介规则="长按网页打开，验证后返回刷新即可";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}else{var 标题规则=".get(h3).t()";var 地址规则=".get(a).a(href)";var 图片规则=".get(img).a(data-src)";var 简介规则=".get(div.categories-wrap).t().tx(&nbsp;).get(span.duration-text).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}
+##搜索翻页
+var b="";for(var i=1;i<50;i=i+1){b=b+"+第"+i+"页="+i;}
+"翻页"+b;
+##免嗅探规则
+JSON.stringify({url:getVar("地址")});
+####
+
+####
+###type
+web
+###分类
+普通站源
+###标题
+风月同天
+###图片
+https://gitcode.net/egwang186/iptv/-/raw/master/onebox/fytt.png
+###BaseURL
+"https://www.fytt1.com";
+###首页地址
+getVar("baseURL")+"/";
+###分类地址
+getVar("baseURL")+"/分类/list翻页.html";
+###搜索地址
+getVar("baseURL")+"/vodsearch/关键字----------翻页---.html";
+###rule
+##首页规则
+var 列表=e2Arr(getVar("源码"),".get(div.stui-vodlist__box)");var 标题规则=".get(h4).t()";var 地址规则=".get(a).a(href)";var 图片规则=".get(a).a(data-original)";var 简介规则=".get(span.pic-text).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##筛选数据
+var a="分类+国产=ChinaPorn+日本=JapanPorn+欧美=WesternPorn+韩国=KoreaPorn+动漫=HentaiPorn+同性=HomosexualPorn";var b="第1页=";for(var i=2;i<999;i++){b=b+"+第"+i+"页="+"_"+i;}var b="翻页+"+b;a+"\n"+b;
+##分类规则
+var 列表=e2Arr(getVar("源码"),".get(div.stui-vodlist__box)");var 标题规则=".get(h4).t()";var 地址规则=".get(a).a(href)";var 图片规则=".get(a).a(data-original)";var 简介规则=".get(span.pic-text).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";
+##选集规则
+var 分类=e2Arr(getVar("源码"),".c()");var 线路="";var 简介="";var 列表规则=".ty(player_data=).json(url).dn(64).dn(utf8)";var 标题规则="";var 选集规则=".var(name)";var 选集地址规则=".c()";
+##搜索规则
+var 源码=getVar("源码");var 列表=e2Arr(源码,".get(div.stui-vodlist__box)");if(!列表[0]){var 列表=["网页可能有搜索验证，去网页看看吧"];var 标题规则=".t()";var 地址规则=URL;var 图片规则=".get(img).a(data-original)";var 简介规则="长按网页打开，验证后返回刷新即可";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}else{var 标题规则=".get(h4).t()";var 地址规则=".get(a).a(href)";var 图片规则=".get(a).a(data-original)";var 简介规则=".get(span.pic-text).t()";var 图片底部规则="";var 左上规则="";var 右上规则="";var NEXTPAGE="";var PREPAGE="";}
+##搜索翻页
+var b="";for(var i=1;i<50;i=i+1){b=b+"+第"+i+"页="+i;}
+"翻页"+b;
+##免嗅探规则
+JSON.stringify({url:getVar("地址")});
+####
+
+#####
