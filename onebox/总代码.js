c28fee1c8c2bf3b6aacc65534e2a98b697796758
1260957445@qq.com <1260957445@qq.com>
2022-09-24 02:21:09 +0800

    g

--- onebox/总代码.js
+++ onebox/总代码.js
@@ -108,12 +108,12 @@ alert("写入失败");
 eval(readStr("QJS"));
 if(getVar("地址").indexOf("远程$")!=-1){
     var u=getVar("地址").split("远程$")[1];
-    var 本地数据=_.read(u.replace(/[:\.\/\?]+/g,"")+".txt");
+    var 本地数据=_.read(u.replace(/[:\.\/\?\s]+/g,"")+".txt");
     if(本地数据.length>500){
         var code=本地数据;
     }else{
         var code=getHttp(u);
-        _.write(code,u.replace(/[:\.\/\?]+/g,"")+".txt");
+        _.write(code,u.replace(/[:\.\/\?\s]+/g,"")+".txt");
     }
 }else{
     var code=getVar("地址");
