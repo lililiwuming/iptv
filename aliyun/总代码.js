0827f2517852d276ab1d4ae710fab547798c35c6
1260957445@qq.com <1260957445@qq.com>
2022-10-03 18:48:40 +0800

    g

--- aliyun/总代码.js
+++ aliyun/总代码.js
@@ -236,8 +236,8 @@ if(getVar("地址").indexOf("$$")!=-1){
             var _d=e2Rex(encodeURI(JSON.stringify(file_data)),".en64()").replace(/\//g,"$");
             var 转码1080='http://116.85.31.19:4000/apis/yun-play/'+_d+'/'+access_token+'/'+share_token+'/FHD/index.m3u8';
             var 转码720='http://116.85.31.19:4000/apis/yun-play/'+_d+'/'+access_token+'/'+share_token+'/HD/index.m3u8';
-            var 测试='http://38.55.98.61:3000/apis/yun-play/'+share_id+'/'+file_id+'/'+access_token+'/'+share_token+'/FHD/index.m3u8';
-        JSON.stringify([{name:"原始文件播放",url:JSON.parse(code).download_url,head:{"Referer":"https://www.aliyundrive.com/"}},{name:"转码1080P可投屏",url:转码1080},{name:"转码720P可投屏",url:转码720},{name:"测试",url:测试}]);
+            var 备用='http://38.55.98.61:3000/apis/yun-play/'+share_id+'/'+file_id+'/'+access_token+'/'+share_token+'/FHD/index.m3u8';
+        JSON.stringify([{name:"原始文件播放",url:JSON.parse(code).download_url,head:{"Referer":"https://www.aliyundrive.com/"}},{name:"不限ip转码",url:备用},{name:"转码1080P可投屏",url:转码1080},{name:"转码720P可投屏",url:转码720}]);
     }
     }
 }else{
@@ -259,7 +259,8 @@ alert("请重新登陆阿里云盘网页");
     var u=getVar("地址").split("?wd=")[1].split("###")[0];
     var 转码1080='http://116.85.31.19:4000/apis/my-yun-play/'+file_id+'/'+drive_id+'/'+access_token+'/FHD/index.m3u8';
     var 转码720='http://116.85.31.19:4000/apis/my-yun-play/'+file_id+'/'+drive_id+'/'+access_token+'/HD/index.m3u8';
-    JSON.stringify([{name:"原始文件播放",url:u,head:{"Referer":"https://www.aliyundrive.com/"}},{name:"转码1080P可投屏",url:转码1080},{name:"转码720P可投屏",url:转码720}]);
+    var 备用='http://38.55.98.61:3000/apis/my-yun-play/'+file_id+'/'+access_token+'/'+share_token+'/FHD/index.m3u8';
+    JSON.stringify([{name:"原始文件播放",url:u,head:{"Referer":"https://www.aliyundrive.com/"}},{name:"不限ip转码",url:备用},{name:"转码1080P可投屏",url:转码1080},{name:"转码720P可投屏",url:转码720}]);
 }
 ######文档预览8
 var cm=android.webkit.CookieManager.getInstance();
