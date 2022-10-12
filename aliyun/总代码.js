2d6f3277d6023df83a0317297471c676741b2e8a
1260957445@qq.com <1260957445@qq.com>
2022-10-12 15:46:55 +0800

    g

--- aliyun/总代码.js
+++ aliyun/总代码.js
@@ -236,8 +236,8 @@ if(getVar("地址").indexOf("$$")!=-1){
             var _d=e2Rex(encodeURI(JSON.stringify(file_data)),".en64()").replace(/\//g,"$");
             var 转码1080='http://116.85.31.19:4000/apis/yun-play/'+_d+'/'+access_token+'/'+share_token+'/FHD/index.m3u8';
             var 转码720='http://116.85.31.19:4000/apis/yun-play/'+_d+'/'+access_token+'/'+share_token+'/HD/index.m3u8';
-            var 备用='http://211.101.244.151:3000/apis/yun-play/'+share_id+'/'+file_id+'/'+access_token+'/'+share_token+'/FHD/index.m3u8';
-            var 备用1='http://198.52.118.199:3000/apis/yun-play/'+share_id+'/'+file_id+'/'+access_token+'/'+share_token+'/FHD/index.m3u8';
+            var 备用='http://113.107.160.110:3000/apis/yun-play/'+share_id+'/'+file_id+'/'+access_token+'/'+share_token+'/FHD/index.m3u8';
+            var 备用1='http://211.101.244.151:3000/apis/yun-play/'+share_id+'/'+file_id+'/'+access_token+'/'+share_token+'/FHD/index.m3u8';
         JSON.stringify([{name:"不限ip转码",url:备用},{name:"原始文件播放",url:JSON.parse(code).download_url,head:{"User-Agent":"okhttp/4.2.2","Connection":"Keep-Alive","Referer":"https://aliyundrive.com/"}},{name:"转码1080P可投屏",url:转码1080},{name:"转码720P可投屏",url:转码720},{name:"不限ip转码",url:备用1}]);
     }
     }
@@ -261,8 +261,8 @@ alert("请重新登陆阿里云盘网页");
     var code=getHttp(JSON.stringify({url:"https://api.aliyundrive.com/v2/file/get_download_url",head:{"Authorization":access_token},postJson:JSON.stringify({drive_id:drive_id,file_id:file_id,expires_sec:0})}));
     var 转码1080='http://116.85.31.19:4000/apis/my-yun-play/'+file_id+'/'+drive_id+'/'+access_token+'/FHD/index.m3u8';
     var 转码720='http://116.85.31.19:4000/apis/my-yun-play/'+file_id+'/'+drive_id+'/'+access_token+'/HD/index.m3u8';
-    var 备用='http://211.101.244.151:3000/apis/my-yun-play/'+file_id+'/'+drive_id+'/'+access_token+'/FHD/index.m3u8';
-    var 备用1='http://198.52.118.199:3000/apis/my-yun-play/'+file_id+'/'+drive_id+'/'+access_token+'/FHD/index.m3u8';
+    var 备用='http://113.107.160.110:3000/apis/my-yun-play/'+file_id+'/'+drive_id+'/'+access_token+'/FHD/index.m3u8';
+    var 备用1='http://211.101.244.151:3000/apis/my-yun-play/'+file_id+'/'+drive_id+'/'+access_token+'/FHD/index.m3u8';
     JSON.stringify([{name:"不限ip转码",url:备用},{name:"原始文件播放",url:JSON.parse(code).internal_url,head:{"User-Agent":"okhttp/4.2.2","Connection":"Keep-Alive","Referer":"https://aliyundrive.com/"}},{name:"转码1080P可投屏",url:转码1080},{name:"转码720P可投屏",url:转码720},{name:"不限ip转码",url:备用1}]);
 }
 ######文档预览8
