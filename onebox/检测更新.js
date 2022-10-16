6743f368c4635d64ef35cf1467a90a207163f6f3
1260957445@qq.com <1260957445@qq.com>
2022-10-17 01:19:58 +0800

    g

--- onebox/检测更新.js
+++ onebox/检测更新.js
@@ -1,11 +1,11 @@
-<a class="版本">220924</a>
+<a class="版本">221017</a>
 <ul class="默认提示">
 <li><font color="blue"><b>已是最新版，本轻站适用最新版土狗浏览器</b></font>
 <p>mqqwpa://im/chat?chat_type=wpa&uin=1260957445</p>
 </li>
 </ul>
 <ul class="更新提示">
-<li><font color="red"><b>有新版本!!![修复直播订阅本地缓存文件重复问题，改为写入本地目录]点击安装</b></font>
+<li><font color="red"><b>有新版本!!![首页订阅写入本地]点击安装</b></font>
 <p>mapi:qm/https://gitcode.net/egwang186/iptv/-/raw/master/onebox/一个盒子.mkz</p>
 </li>
 </ul>
