bff628e7c9ab3e420a2e90ad79c2a3507831ce2c
1260957445@qq.com <1260957445@qq.com>
2023-01-30 18:37:55 +0800

    g

--- onebox/总代码.js
+++ onebox/总代码.js
@@ -137,6 +137,15 @@ function 选集列表(){
             let 选集地址=e2Rex(列表[j],选集地址规则);
             LIST.push({title:选集,url:选集地址});
         }
+    return {title:标题,list:LIST};
+    };
+    }
+    d.push(fn(index));
+  }
+  var s = _.submit(d, 分类.length); //n 改为你想开启的线程数
+  for (let i = 0; i < s.length; i++) {
+    for (let z of s[i].get()) {
+        let LIST=z.list;
         let obj = {}
         let addLIST = []
         LIST.forEach(({name, value}) => {
@@ -153,14 +162,7 @@ function 选集列表(){
            addLIST.push({name, value})
            }
         })
-    return {title:标题,list:addLIST};
-    };
-    }
-    d.push(fn(index));
-  }
-  var s = _.submit(d, 分类.length); //n 改为你想开启的线程数
-  for (let i = 0; i < s.length; i++) {
-    for (let z of s[i].get()) {
+        z.list=addLIST;
       if(z.list.length/100 > 1){
         var zz=[];
         for (let j = 0; j < z.list.length;){
