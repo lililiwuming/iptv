db11b3ad01ecc3126609a769778d3b7c68c33cfb
1260957445@qq.com <1260957445@qq.com>
2023-03-05 22:22:18 +0800

    g

--- aliyun/总代码.js
+++ aliyun/总代码.js
@@ -445,3 +445,206 @@ JSON.stringify(items);
 }
 ######QJS12
 KGZ1bmN0aW9uKCl7dmFyIF89e307dmFyIG09MS4wO3ZhciByPWFuZHJvaWQub3MuRW52aXJvbm1lbnQuZ2V0RXh0ZXJuYWxTdG9yYWdlRGlyZWN0b3J5KCk7dmFyIHM9YW5kcm9pZC53ZWJraXQuTWltZVR5cGVNYXAuZ2V0U2luZ2xldG9uKCk7dmFyIHQ9amF2YS5pbzt2YXIgdT1qYXZhLmxhbmc7dmFyIHY9dC5GaWxlLnNlcGFyYXRvcjt2YXIgdz1wYXRoKCkrdisnRG93bmxvYWQnK3YrJ3FtJyt2K2luZm8oKS5zaWduLnN1YnN0cmluZygwLDQpKyckJCQnK2luZm8oKS5uYW1lK3Y7aXNTdHJpbmc9KHZhbCk9PnR5cGVvZiB2YWw9PT0nc3RyaW5nJztmdW5jdGlvbiBpc09iamVjdChhKXtjb25zdCB0eXBlPXR5cGVvZiBhO3JldHVybiBhIT1udWxsJiYodHlwZT09J29iamVjdCd8fHR5cGU9PSdmdW5jdGlvbicpfXZhciB5PWphdmEudXRpbC5jb25jdXJyZW50O3ZhciB6PW5ldyB5LkV4ZWN1dG9ycy5uZXdDYWNoZWRUaHJlYWRQb29sKCk7ZnVuY3Rpb24gdGhyZWFkKGEpe29iaj17Y2FsbDpmdW5jdGlvbigpe3JldHVybiBhKCl9LH07dmFyIGI9bmV3IHkuRnV0dXJlVGFzayhvYmopO3ouc3VibWl0KGIpO3JldHVybiBifWZ1bmN0aW9uIGNodW5rKGEsYil7bGV0IGluZGV4PTA7bGV0IHJlcz1bXTt3aGlsZShpbmRleDxhLmxlbmd0aCl7cmVzLnB1c2goYS5zbGljZShpbmRleCxpbmRleCtiKSk7aW5kZXgrPWJ9cmV0dXJuIHJlcy5maWx0ZXIoKG8pPT5vLmxlbmd0aD4wKX1mdW5jdGlvbiBzdWJtaXQobCxuKXtjPVtdO2xpc3Q9Y2h1bmsobCxsLmxlbmd0aC9uKTt2YXIgbj0wO3doaWxlKG48bGlzdC5sZW5ndGgpe2Z1bmN0aW9uIGwoeCl7cmV0dXJuIGZ1bmN0aW9uIG8oKXt2YXIgYT1bXTtmb3IobGV0IGk9MDtpPGxpc3RbeF0ubGVuZ3RoO2krKyl7YS5wdXNoKGxpc3RbeF1baV0oKSl9cmV0dXJuIGF9fWMucHVzaCh0aHJlYWQobChuKSkpO24rK31yZXR1cm4gY31mdW5jdGlvbiBta2RpcnMoZil7dmFyIGE9Zi5nZXRQYXJlbnRGaWxlKCk7aWYoIWEuZXhpc3RzKCkpYS5ta2RpcnMoKX1mdW5jdGlvbiB3cml0ZShhLGIpe3ZhciBjPW5ldyB0LkZpbGUodysn5pWw5o2uJyt2K2IpO21rZGlycyhjKTt2YXIgZD1uZXcgdC5GaWxlV3JpdGVyKGMsZmFsc2UpO2Qud3JpdGUoYSk7ZC5jbG9zZSgpfWZ1bmN0aW9uIHJlYWQoYSl7dmFyIGI9bmV3IHQuRmlsZSh3KyfmlbDmja4nK3YrYSk7dmFyIGM9MDtpZighYi5leGlzdHMoKXx8KGM9Yi5sZW5ndGgoKSk9PTApcmV0dXJuJyc7dmFyIGQ9dS5yZWZsZWN0LkFycmF5Lm5ld0luc3RhbmNlKHUuQnl0ZS5UWVBFLGMpO3ZhciBlPW5ldyB0LkZpbGVJbnB1dFN0cmVhbShiKTtlLnJlYWQoZCk7ZS5jbG9zZSgpO3JldHVybiBuZXcgdS5TdHJpbmcoZCl9dmFyIEE9UGFja2FnZXMub3JnLmpzb3VwO3ZhciBCPUEuQ29ubmVjdGlvbi5NZXRob2Q7ZnVuY3Rpb24gaHR0cChvKXtsZXR7dXJsLGhlYWRlcixwYXJhbXMsanNvbixjaGFyc2V0LG1ldGhvZCxyZX09bzt2YXIgYT1BLkpzb3VwLmNvbm5lY3QodXJsKTthLmlnbm9yZUNvbnRlbnRUeXBlKHRydWUpO2EucG9zdERhdGFDaGFyc2V0KGNoYXJzZXQpO2EubWF4Qm9keVNpemUoMTA0ODU3NjAwMCk7cmU9PT1mYWxzZT9hLmZvbGxvd1JlZGlyZWN0cyhyZSk6YS5mb2xsb3dSZWRpcmVjdHModHJ1ZSk7aWYoaXNPYmplY3QoaGVhZGVyKSlmb3IoeCBpbiBoZWFkZXIpYS5oZWFkZXIoeCxoZWFkZXJbeF0pO2lmKGlzT2JqZWN0KHBhcmFtcykpe2lmKGpzb249PT10cnVlKWEucmVxdWVzdEJvZHkocGFyYW1zKTtlbHNlIGZvcihwIGluIHBhcmFtcylhLmRhdGEocCxwYXJhbXNbcF0pfXZhciBiO2lmKGpzb249PT10cnVlfHxtZXRob2Q9PSdwb3N0JyliPWEubWV0aG9kKEIuUE9TVCkuZXhlY3V0ZSgpO2Vsc2UgYj1hLm1ldGhvZChCLkdFVCkuZXhlY3V0ZSgpO3JldHVybiBifWZ1bmN0aW9uIGh0dHBCb2R5KGEsYil7YS5jaGFyc2V0KGIpO3JldHVybiBhLmJvZHkoKX1mdW5jdGlvbiBodHRwQ29va2llKGEsYil7cmV0dXJuIGIrJz0nK2EuY29va2llKGIpfWZ1bmN0aW9uIGh0dHBDb29raWVzKGEpe3ZhciBiPScnO3ZhciBjPWEuY29va2llcygpLmVudHJ5U2V0KCkuaXRlcmF0b3IoKTt3aGlsZShjLmhhc05leHQoKSl7dmFyIGQ9Yy5uZXh0KCk7Yis9ZC5nZXRLZXkoKSsnPScrZC5nZXRWYWx1ZSgpKyc7J31yZXR1cm4gYn1mdW5jdGlvbiBodHRwSGVhZGVyKGEsYil7cmV0dXJuIGEuaGVhZGVyKGIpfWZ1bmN0aW9uIGh0dHBIZWFkZXJzKGEpe3ZhciBiPScnO3ZhciBjPWEuaGVhZGVycygpLmVudHJ5U2V0KCkuaXRlcmF0b3IoKTt3aGlsZShjLmhhc05leHQoKSl7dmFyIGQ9Yy5uZXh0KCk7Yis9ZC5nZXRLZXkoKSsnPScrZC5nZXRWYWx1ZSgpKyc7J31yZXR1cm4gYn1mdW5jdGlvbiB0cmltVShhKXt2YXIgYj1hLmxhc3RJbmRleE9mKCcvJyk7aWYoYS5sZW5ndGgoKT09YisxKXthPWEuc3Vic3RyaW5nKDAsYik7cmV0dXJuIHRyaW1VKGEpfXJldHVybiBhLnN1YnN0cmluZygwLGEubGFzdEluZGV4T2YoJy4nKSl9ZnVuY3Rpb24gZG93bmxvYWQobyl7dHJ5e2xldHtzZXRwYXRoLHRpcHN9PW87dmFyIGE9aHR0cChvKTt2YXIgYj10cmltVShhLnVybCgpLnRvU3RyaW5nKCkpO3ZhciBjPXMuZ2V0RXh0ZW5zaW9uRnJvbU1pbWVUeXBlKGEuY29udGVudFR5cGUoKS5zcGxpdCgnOycpWzBdKTt2YXIgZD1iLnN1YnN0cmluZyhiLmxhc3RJbmRleE9mKCcvJykrMSkrJy4nK2M7c2F2ZXBhdGg9aXNTdHJpbmcoc2V0cGF0aCk/c2V0cGF0aCt2K2Q6dysn5LiL6L29Jyt2K2Q7dmFyIGY9YS5ib2R5U3RyZWFtKCk7dmFyIGc9bmV3IHUucmVmbGVjdC5BcnJheS5uZXdJbnN0YW5jZSh1LkJ5dGUuVFlQRSw0MDk2KTt2YXIgaD0wO3ZhciBpPW5ldyB0LkJ5dGVBcnJheU91dHB1dFN0cmVhbSgpO3doaWxlKChoPWYucmVhZChnKSkhPS0xKXtpLndyaXRlKGcsMCxoKX12YXIgaj1uZXcgdC5GaWxlKHNhdmVwYXRoKTtta2RpcnMoaik7dmFyIGs9bmV3IHQuRmlsZU91dHB1dFN0cmVhbShqKTtrLndyaXRlKGkudG9CeXRlQXJyYXkoKSk7aWYodGlwcz09PWZhbHNlKXJldHVybiBzYXZlcGF0aDt0b2FzdCgn5LiL6L295oiQ5Yqf77yM6Lev5b6EOicrc2F2ZXBhdGgpO3JldHVybiBzYXZlcGF0aH1jYXRjaChlKXtlcnJvcihlKTt0b2FzdCgn5LiL6L295aSx6LSlLOivt+aJk+W8gOiwg+ivleWPsOafpeeci+WFt+S9k+W8guW4uOS/oeaBrycpfWZpbmFsbHl7aWYoaSE9bnVsbClpLmNsb3NlKCk7aWYoayE9bnVsbClrLmNsb3NlKCk7aWYoZiE9bnVsbClmLmNsb3NlKCl9fWZ1bmN0aW9uIGVycm9yKGUpe2lzU3RyaW5nKGUpP+aKpemUmShlKTrmiqXplJkoZS50b1N0cmluZygpKX1mdW5jdGlvbiB0b2FzdChhKXthbGVydChhKX1mdW5jdGlvbiBiYWNrKCl7bmV3IFBhY2thZ2VzLmFuZHJvaWQuYXBwLkluc3RydW1lbnRhdGlvbigpLnNlbmRLZXlEb3duVXBTeW5jKDQpfWZ1bmN0aW9uIHBhdGgoKXtyZXR1cm4gci5nZXRBYnNvbHV0ZVBhdGgoKS50b1N0cmluZygpfWZ1bmN0aW9uIHNsZWVwKGEpe2phdmEubGFuZy5UaHJlYWQuc2xlZXAoYSl9ZnVuY3Rpb24gaW5mbygpe289e307by5zaWduPWUyUmV4KGdldFZhcignUU1JTkZPJyksJy5nZXQoc2lnbikudCgpJyk7by52ZXJzaW9uPWUyUmV4KGdldFZhcignUU1JTkZPJyksJy5nZXQodmVyc2lvbikudCgpJyk7by5uYW1lPWUyUmV4KGdldFZhcignUU1JTkZPJyksJy5nZXQobmFtZSkudCgpJyk7cmV0dXJuIG99Xy5WRVJTSU9OPW07Xy5yZWFkPXJlYWQ7Xy53cml0ZT13cml0ZTtfLnNsZWVwPXNsZWVwO18udGhyZWFkPXRocmVhZDtfLnN1Ym1pdD1zdWJtaXQ7Xy5jaHVuaz1jaHVuaztfLmJhY2s9YmFjaztfLmlzU3RyaW5nPWlzU3RyaW5nO18uaHR0cD1odHRwO18uYmQ9aHR0cEJvZHk7Xy5jaz1odHRwQ29va2llO18uY2tzPWh0dHBDb29raWVzO18uaGQ9aHR0cEhlYWRlcjtfLmhkcz1odHRwSGVhZGVycztfLmRvd25sb2FkPWRvd25sb2FkO18uaW5mbz1pbmZvO18udG9hc3Q9dG9hc3Q7Xy5lcnJvcj1lcnJvcjt0aGlzLl89X30uY2FsbCh0aGlzKSk7
+######直播选集列表13
+eval(readStr("QJS"));
+function read(path) {
+    var b = new java.io.File(path);
+    var c = 0;
+    if (!b.exists() || (c = b.length()) == 0) return '';
+    var d = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, c);
+    var e = new java.io.FileInputStream(b);
+    e.read(d);
+    e.close();
+    return new java.lang.String(d)
+}
+    var path=getVar("地址").split("?tugourl=")[1].split("$tugopath$")[0];
+    var code=read(path);
+function 选集列表(){
+    var res={};var items=[];
+    var d = [];
+    for (let index = 0; index < 分类.length; index++) {
+            function fn(i) {
+              return function () {
+        var 分类CODE=分类[i];
+        var 列表=e2Arr(分类CODE,列表规则).filter(Boolean);
+        if(线路){
+        var 标题=e2Rex(线路[i],标题规则);
+        }else{
+        var 标题=e2Rex(分类CODE,标题规则);
+        }
+        var LIST=[];
+        for(let j=0;j<列表.length;j++){
+            let 选集=e2Rex(列表[j],选集规则);
+            let 选集地址=e2Rex(列表[j],选集地址规则);
+            LIST.push({title:选集,url:选集地址});
+        }
+    return {title:标题,list:LIST};
+    };
+    }
+    d.push(fn(index));
+  }
+  var s = _.submit(d, 分类.length); //n 改为你想开启的线程数
+  for (let i = 0; i < s.length; i++) {
+    for (let z of s[i].get()) {
+      if(z.list.length/100 > 1){
+        var zz=[];
+        for (let j = 0; j < z.list.length;){
+            zz.push(z.list.slice(j, j+=100));
+        }
+        for(let k=0;k<zz.length;k++){
+            let 尾=(k+1)*100;
+            let 头=尾-100+1;
+            if(尾>z.list.length){
+                尾=z.list.length;
+            }
+            items.push({title:z.title+"|"+zz[k][0].title,list:zz[k]});
+        }
+      }else{
+        items.push(z);
+      }
+    }
+  }
+    res.data=items;
+    return JSON.stringify(res);
+}
+if(code.indexOf("#genre#")!=-1){
+    var 分类=code.split(/.+?#genre#.*/).filter(item=>item.indexOf("://")!=-1);
+    var 线路=code.match(/.+?#genre#.*/g);
+    var 列表规则=".z(.+?,.+?://.+)";
+    var 标题规则=".tz(,#genre#)";
+    var 选集地址规则=".c(http://ip111.cn/?wd=).ty(,)";
+    var 选集规则=".tz(,)";选集列表();
+}else if(code.indexOf("#EXTINF:")!=-1){
+    var code=code.match(/#EXTINF:.+[\s]+[^#"]+?:\/\/.+/g);
+    var res={};var items=[];var d=[];
+    for (let index = 0; index < code.length; index++) {
+        function fn(i) {
+          return function () {
+            if(code[i].indexOf(",")!=-1){
+                var 选集=code[i].match(/,(.*)/)[1]||"无选集名称";var 选集地址=code[i].match(/,.*[\s]+(.+)/)[1]||"无播放地址";
+            }else{
+                var 选集=code[i].match(/.+"(.*)/)[1]||"无选集名称";var 选集地址=code[i].match(/.+[\s]+(.+)/)[1]||"无播放地址";
+            }
+    if(code[i].search(/group-title=".*?"/)!=-1){
+        var type=code[i].match(/group-title="(.*?)"/)[1]||"不规范分类";
+    }else{
+        var type="未分类";
+    }
+    var 当前条目=[];当前条目.push({title:选集,url:"http://ip111.cn/?wd="+选集地址});
+        return {title:type,list:当前条目};
+    };
+    }
+    d.push(fn(index));
+  }
+var s=_.submit(d, code.length); //n 改为你想开启的线程数
+for (let i = 0; i < s.length; i++) {
+    for (let z of s[i].get()) {
+        if(items.length==0) {
+            items.push(z);
+        }else{
+            let 寻找=items.some(item=>{
+            //判断类型，有就添加到当前项
+              if(item.title == z.title&&item.list.length<100){
+              item.list=item.list.concat(z.list);
+              return true
+              }
+            });
+            if (!寻找) {
+            //如果没找相同类型添加一个类型
+            items.push(z);
+            }
+        }
+    }
+  }
+res.data=items;
+JSON.stringify(res);
+}else if(code.search(/\$c_start.+?\$c_end/)!=-1){
+    var 分类=code.split(/\$c_start.+?\$c_end/).filter(item=>item.indexOf("://")!=-1);
+    var 线路=code.match(/\$c_start.+?\$c_end/g);
+    var 列表规则=".z(.+?,.+?://.+)";
+    var 标题规则=".ty(c_start).tz($c_end)";
+    var 选集地址规则=".c(http://ip111.cn/?wd=).ty(,)";
+    var 选集规则=".tz(,)";选集列表();
+}else{
+    var code=code.match(/.+?,.+?:\/\/.+/g);
+    var res={};var items=[];var d=[];
+    for (let index = 0; index < code.length; index++) {
+        function fn(i) {
+          return function () {
+    var 选集=code[i].match(/(.+),/)[1];var 选集地址=code[i].match(/,[\s]*?(.+)/)[1];
+    if(code[i].indexOf("|")!=-1){
+        var type=选集.split("|")[0];
+        var 选集标题=选集.split("|")[1];
+    }else{
+        var type=getVar("标题")+"-无子分类";
+        var 选集标题=选集;
+    }
+    var 当前条目=[];当前条目.push({title:选集标题,url:"http://ip111.cn/?wd="+选集地址});
+    return {title:type,list:当前条目};
+};
+}
+d.push(fn(index));
+}
+var s=_.submit(d, code.length); //n 改为你想开启的线程数
+for (let i = 0; i < s.length; i++) {
+for (let z of s[i].get()) {
+    if(items.length==0) {
+        items.push(z);
+    }else{
+        let 寻找=items.some(item=>{
+        //判断类型，有就添加到当前项
+          if(item.title == z.title&&item.list.length<100){
+          item.list=item.list.concat(z.list);
+          return true
+          }
+        });
+        if (!寻找) {
+        //如果没找相同类型添加一个类型
+        items.push(z);
+        }
+    }
+}
+}
+res.data=items;
+JSON.stringify(res);
+}
+######直播免嗅探14
+var uu=getVar("地址").split("/?wd=")[1];
+if(uu.indexOf("#")!=-1){
+var urls=uu.split("#");
+var items=[];
+for(var i=0;i<urls.length;i++){
+    if(urls[i].indexOf(".php")!=-1){
+        var resp = JZ(JSON.stringify({ url: urls[i], redirect: false }));
+        var u = resp.head.Location || resp.head.location;
+        if(!u){
+            u=urls[i]+"&type=.m3u8";
+        }
+    }else if(urls[i].indexOf("mitv://")!=-1){
+        var u=urls[i].replace("mitv://","P2p://");
+    }else if(urls[i].indexOf("www.youtube.com/watch?v=")!=-1){
+        var resp=getHttp(JSON.stringify({url:"https://www.azrotv.com/extras/youtube/",post:{"url":urls[i]}}));
+        var u=e2Rex(resp,".get(textarea).t()");
+    }else{
+        var u=urls[i];
+    }
+    items.push({name:"地址"+(i+1),url:u}); 
+}
+JSON.stringify(items);
+}else{
+    if(uu.indexOf(".php")!=-1){
+        var resp=JZ(JSON.stringify({url:uu,redirect:false}));
+        var u=resp.head.Location||resp.head.location;
+        if(!u){
+            u=uu+"&type=.m3u8";
+        }
+    }else if(uu.indexOf("mitv://")!=-1){
+        var u=uu.replace("mitv://","P2p://");
+    }else if(uu.indexOf("www.youtube.com/watch?v=")!=-1){
+        var resp=getHttp(JSON.stringify({url:"https://www.azrotv.com/extras/youtube/",post:{"url":uu}}));
+        var u=e2Rex(resp,".get(textarea).t()");
+    }else{
+        var u=uu;
+    }
+JSON.stringify({name:"地址",url:u});
+}
