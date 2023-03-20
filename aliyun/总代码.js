86d1115dca6b7261d4184dacfba8c2f49b02f327
1260957445@qq.com <1260957445@qq.com>
2023-03-20 23:13:44 +0800

    G

--- aliyun/总代码.js
+++ aliyun/总代码.js
@@ -313,7 +313,7 @@ var HEAD=JSON.stringify({"Authorization":access_token});
     var u=getVar("地址").split("?wd=")[1].split("###")[0];
     var xincode=getHttp(JSON.stringify({url:"https://api.aliyundrive.com/adrive/v1/file/get_path",postJson:JSON.stringify({drive_id:drive_id,file_id:file_id}),head:JSON.parse(HEAD)}));
     var 转码HQ='http://113.107.160.110:3000/apis/my-yun-audio/'+file_id+'/'+drive_id+'/'+access_token+'/LQ/master.mp3';
-    JSON.stringify([{name:"原始文件",url:JSON.parse(xincode).items[0].download_url,head:{"Referer":"https://www.aliyundrive.com/"}},{name:"LQ低音质转码",url:转码HQ,head:{"Referer":"https://www.aliyundrive.com/"}}]);
+    JSON.stringify([{name:"原始文件",url:JSON.parse(xincode).items[0].url,head:{"Referer":"https://www.aliyundrive.com/"}},{name:"LQ低音质转码",url:转码HQ,head:{"Referer":"https://www.aliyundrive.com/"}}]);
     }else{
         var file_id=getVar("地址").split("?wd=")[1].split("###")[2];
         var drive_id=getVar("地址").split("?wd=")[1].split("###")[1];
